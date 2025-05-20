31f284148013cedd9b19db88acd8677d3478fbfa
sankethR102 <sanketh.ravishankar@gmail.com>
2025-05-20 12:13:20 +0530

    Initial commit

---
+++ src/main/java/com/example/app/App.java
@@ -0,0 +1,11 @@
+package com.example.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven Project!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/example/app/AppTest.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testAdd() {
+        App app = new App();
+        assertEquals(5, app.add(2, 3));
+    }
+}
